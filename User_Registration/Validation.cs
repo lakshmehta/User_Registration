f05791fb924685be6368ba2f35e1aff29df9408e
unknown <lakshmehta06@gmail.com>
2021-04-15 23:05:03 +0530

    [LAKSH ADDED UC3]

--- User_Registration/Validation.cs
+++ User_Registration/Validation.cs
@@ -9,6 +9,7 @@ namespace User_Registration
     {
         public const string Regex_FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
         public const string Regex_LastName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public const string Regex_Email = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
         public bool ValidateFirstName(string FirstName)
         {
             return Regex.IsMatch(FirstName, Regex_FirstName);
@@ -16,6 +17,10 @@ namespace User_Registration
         public bool ValidateLastName(string LastName)
         {
             return Regex.IsMatch(LastName, Regex_LastName);
+        }
+        public bool ValidateEmail(string Email)
+        {
+            return Regex.IsMatch(Email, Regex_Email);
         }
     }
 }
