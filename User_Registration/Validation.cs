2e981418dc3b0cae90ae61899e227cbdd038b538
unknown <lakshmehta06@gmail.com>
2021-04-15 23:08:00 +0530

    [LAKSH ADDED UC4]

--- User_Registration/Validation.cs
+++ User_Registration/Validation.cs
@@ -10,6 +10,7 @@ namespace User_Registration
         public const string Regex_FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
         public const string Regex_LastName = "^[A-Z]{1}[A-Za-z]{2,}$";
         public const string Regex_Email = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        public const string Regex_MobileNumber = "^[1-9]{1}[0-9]{0,3}\\s[1-9]{1}[0-9]{9}$";
         public bool ValidateFirstName(string FirstName)
         {
             return Regex.IsMatch(FirstName, Regex_FirstName);
@@ -21,6 +22,10 @@ namespace User_Registration
         public bool ValidateEmail(string Email)
         {
             return Regex.IsMatch(Email, Regex_Email);
+        }
+        public bool ValidateMobile(string MobileNumber)
+        {
+            return Regex.IsMatch(MobileNumber, Regex_MobileNumber);
         }
     }
 }
